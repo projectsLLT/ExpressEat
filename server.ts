e3271362fa2e272a212f858bc2af1aedbaf0bb7e
lucio-souza <desouzaleitelucioweslley@gmail.com>
2024-09-01 18:38:10 -0300

    criando a conexao com o mongodb e o model do usuario

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoDb from './src/database/mongodbDatabase';
 
 const app=express();
 const port=process.env.Port;
---
+++ src/model/Cliente.ts
@@ -0,0 +1,46 @@
+import mongoDb from "../database/mongodbDatabase";
+import {v4 as uuid} from 'uuid'
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema= new Schema({
+    _id:{
+        type:String,
+        default:uuid()
+    },
+    nome:{
+        type:String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    idade:{
+        type:Number
+    },
+    localzacao:{
+        type: {
+            type: String,
+            enum: ['Point'], 
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        }
+    },
+    senha:{
+        type:String,
+        required:true
+    },
+    cpf:{
+        type:String,
+        unique:true,
+        required:true
+    }
+});
+
+const User=mongoDb.model('Usuario',UserSchema);
+
+export default User;
